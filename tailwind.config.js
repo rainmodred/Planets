c3715c8c4f9ef6f0a8f843c14b897fda0adff79b
rainmodred <albphoenix@gmail.com>
2021-12-22 19:52:42 +0300

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  safelist: [
+    {
+      pattern:
+        /^(bg|border)-(mercury|venus|earth|mars|jupiter|saturn|uranus|neptune)$/,
+    },
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -15,6 +21,21 @@ module.exports = {
       },
       letterSpacing: {
         custom: '-1.05px',
+      },
+      colors: {
+        'white-100': 'rgba(255, 255, 255, 0.1)',
+        'white-700': 'rgba(255, 255, 255, 0.75)',
+        blackMain: 'rgb(7, 7, 36)',
+        darkGrey: 'rgb(56, 56, 79)',
+        lightGrey: 'rgb(131, 131, 145)',
+        mercury: 'hsl(196, 83%, 93%)',
+        venus: 'hsl(39, 88%, 73%)',
+        earth: 'hsl(238, 99%, 66%)',
+        mars: 'hsl(12, 100%, 64%)',
+        jupiter: 'hsl(27, 75%, 70%)',
+        saturn: 'hsl(40, 96%, 70%)',
+        uranus: 'hsl(168, 82%, 67%)',
+        neptune: 'hsl(222, 95%, 63%)',
       },
     },
   },
