3df7030d8d5e73315b6c0d8a3b659f0609522018
rainmodred <albphoenix@gmail.com>
2021-12-31 17:55:34 +0300

    Add page title

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,10 +1,17 @@
+import Head from 'next/head';
 import Header from '../Header/Header';
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col justify-center min-h-screen">
-      <Header />
-      <main className="grid mt-2 md:mt-12 grow pb-6">{children}</main>
-    </div>
+    <>
+      <Head>
+        <title>Planets</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="flex flex-col justify-center min-h-screen">
+        <Header />
+        <main className="grid mt-2 md:mt-12 grow pb-6">{children}</main>
+      </div>
+    </>
   );
 }
